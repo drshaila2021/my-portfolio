fe80ae81ebe7a1f1c5bd972421d3c1cce132c402
Shaila Nasrin <drshaila2021@gmail.com>
2022-06-08 22:03:38 -0500

    first

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h3 className="text-center">Home Page</h3>
+    </div>
+  );
+};
+
+export default Home;
